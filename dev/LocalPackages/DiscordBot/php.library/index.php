0d0860085267020573e937eb2970d644f1409911
cypher139 <xmproductions13@gmail.com>
2023-04-27 21:57:33 -0700

    init Leaderboard website
    Some quick PHP additions to support displaying json leaderboard data from the bot.
    
    json data hidden behind a random folder name for some quick 'security through obscurity'. lol
    
    More optimizations to follow (such as viewing removed data)

---
+++ dev/LocalPackages/DiscordBot/php.library/index.php
@@ -0,0 +1,78 @@
+<html>
+<?php
+//Version 0.9
+	$sortType = 'level';
+	if(array_key_exists('sort', $_GET)) {
+		$sortType = $_GET['sort'];
+	}
+	$limit = 0;
+	if(array_key_exists('limit', $_GET)) {
+		$limit = $_GET['limit'];
+	}
+	function doSort($a,$b) {
+		global $sortType;
+		return $a->{$sortType} < $b->{$sortType};
+	};
+	 
+    $json = file_get_contents("{$secretKey}/Leaderboard.json");
+	$boardType = 'Leaderboard';
+	if(array_key_exists('removed', $_GET)) {
+		$json = file_get_contents("{$secretKey}/Leaderboard_Removed.json");
+		$boardType = 'Leaderboard (Removed Users)';
+	}
+	$xpjson = file_get_contents("Leaderboard_LevelXP.json");
+    $levelxp = json_decode($xpjson);
+	$data = get_object_vars(json_decode($json));
+	$guild = get_object_vars($data['guild']);
+    $members = get_object_vars($data['members']);
+	$entrycount = 0;
+     uasort($members, "doSort");
+?>
+	<head>
+	<title><?php echo $data['LB_Guild_Name']; ?> <?php echo $boardType ?></title>
+	<link rel="icon" type="image/x-icon" href="../assets/favicon.ico">
+	<link rel="stylesheet" type="text/css" href="../assets/theme.css">
+</head>
+
+<body>
+	<img src="../assets/TMClogo.png" />&nbsp;<b><?php echo $data['LB_Guild_Name']; ?> <?php echo $boardType ?></b>&nbsp;&nbsp;&nbsp;&nbsp;<a href="../index.html">Main Site</a>
+	<hr />
+	<?php echo $boardType ?>&nbsp;&nbsp;&nbsp;&nbsp;<em>Sorted by <?php echo $sortType ?> </em><br />
+<table>
+    <tr>
+        <td><a href="index.php?sort=username">Username</a></td>
+		<td><a href="index.php?sort=level">Level</a></td>
+        <td><a href="index.php?sort=xp">XP</a></td>
+        <td><a href="index.php?sort=msg_count">Messages</a></td>
+		<td>XP to next level</td>
+    </tr>
+    <?php
+        foreach ($members as $key => $row){
+			$level = $row->level;
+			if($level == 0) { continue; }
+            $xp = $row->xp;
+            $username = $row->username;
+            $msg_count = $row->msg_count;
+			$entrycount++;
+    ?>
+    <tr>
+        <td><?php echo $username ?></td>
+		<td><?php echo $level ?></td>
+        <td><?php echo $xp ?></td>
+        <td><?php echo $msg_count ?></td>
+		<td><?php 
+			$xpleft = $levelxp[($level + 1)] - $xp;
+		
+		echo $xpleft ?></td>
+    </tr>
+    <?php 
+			if($limit != 0) {
+				if($entrycount == $limit) {
+					break;
+				}
+			}
+	} ?>
+</table>
+	<em>Showing</em> <?php echo $entrycount ?> <em>of</em> <?php echo count(array_keys($members)); ?> <em>entries.</em>
+</body>
+</html>
