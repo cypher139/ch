58f82d8c00a19e32a5c1bf307553df7181bce431
cypher139 <xmproductions13@gmail.com>
2024-08-29 15:17:27 -0700

    Bot: leaderboard json, other misc updates
    - debug: check former members
    - member join: do not award admin roles / permissions checks
    - leaderboard feature: add path to publish leaderboard json data to, for webserver to pull
    -webhook fixes
    -some file renames
    -checkforbots command fixes. Trying to pull from kickthespy.pet/ids results in a 403 error, so manual id download is still used.

--- dev/DiscordBot/CommandLine_StartBot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-java --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.prefs/java.util.prefs=ALL-UNNAMED -Xrs -jar commandhelper.jar cmdline MethodScript/LocalPackages/DiscordBot/DiscordBot.ms 
-
