7834cf344414dc45c6ab601f4aa535229e42f2a9
cypher139 <xmproductions13@gmail.com>
2025-05-19 11:55:24 -0700

    IPBlacklist: Database Update, when!?
    Changes downloading / updating database files to not be a part of the query, as blocking was not occurring and python would error out with can't open invalid file.
    
    Now checks for updates weekly, and will download databases only if py reported them missing or corrupt.
    
    Also adds some message prefix updates  and usual IP updates

--- dev/Chat/geoip.py
+++ dev/Chat/geoip.py
@@ -3,29 +3,35 @@ import sys
 import json
 
 x = 1
-ispdatajson = {}
+ispdatajson = {'updateFile': []}
 
 for arg in sys.argv[1:]:
 
     ipa = str(sys.argv[x])
     ispdatajson[ipa] = {}
 
-    with maxminddb.open_database('GeoLite2-City.mmdb') as reader:
-        record = reader.get(ipa)
+    try:
+        with maxminddb.open_database('GeoLite2-City.mmdb') as reader:
+            record = reader.get(ipa)
+        try:
+            ispdatajson[ipa].update(record)
+        except (TypeError, KeyError):
+            pass        
+    except (FileNotFoundError, maxminddb.errors.InvalidDatabaseError):
+        ispdatajson['updateFile'].append('City')
 
+    try:
+        with maxminddb.open_database('GeoLite2-ASN.mmdb') as reader:
+            record = reader.get(ipa)
         try:
             ispdatajson[ipa].update(record)
         except (TypeError, KeyError):
             pass
-
-    with maxminddb.open_database('GeoLite2-ASN.mmdb') as reader:
-        record = reader.get(ipa)
-
-        try:
-            ispdatajson[ipa].update(record)
-        except (TypeError, KeyError):
-            pass
+    except (FileNotFoundError, maxminddb.errors.InvalidDatabaseError):
+        ispdatajson['updateFile'].append('ASN')
 
     x += 1
+    if len(ispdatajson['updateFile']) > 0:
+        break
 
 print(json.dumps(ispdatajson))
